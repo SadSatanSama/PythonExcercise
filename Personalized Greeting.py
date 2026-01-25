f46ad64feb9f24d172ed53cb8fade61d1fa86e56
SadSatanSama <arijit08das@gmail.com>
2026-01-25 21:07:15 +0530

    initial commit

---
+++ Personalized Greeting.py
@@ -0,0 +1,4 @@
+print ("This is the Personalized Greeting Message for Assessment")
+name=input("Enter your first name: ")
+surname=input ("Enter your last name: ")
+print ("Hello,", name, surname, "! Welcome to the Python program.")
