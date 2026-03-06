c4e62cef87c7ed4df870fcbcfb45a2d635f189d4
SadSatanSama <arijit08das@gmail.com>
2026-03-06 10:54:36 +0530

    new assignment

---
+++ Arithmatic_progression.py
@@ -0,0 +1,5 @@
+num_sum=0
+for i in range (1, 51):
+    print (i, end=" ")
+    num_sum+=i
+print(f"\nThe sum of the numbers from 1 to 50 is: {num_sum}")
---
+++ Even_Odd_Numbers.py
@@ -0,0 +1,10 @@
+# Check if the Number is Even or Odd
+print("EVEN/ODD NUMBER CHECKER")
+
+num=int(input("Enter a number: "))
+    
+if num%2==0:
+    print(f"{num} is an even number.")
+
+else:
+    print(f"{num} is an odd number.")
